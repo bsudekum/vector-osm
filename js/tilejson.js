fe7db0d3baa7b5aace891753a7287ababe3b6272
rsudekum <bobby@mapbox.com>
2013-06-26 17:22:11 -0400

    added topojson

--- js/tilejson.js
+++ js/tilejson.js
@@ -9,45 +9,79 @@
  *   Match D3 idioms for .classed(), .style(), etc
  *   Work on allowing feature popups, etc.
  */
-L.TileLayer.d3_geoJSON =  L.TileLayer.extend({
+L.TileLayer.d3_topoJSON =  L.TileLayer.extend({
     onAdd : function(map) {
         L.TileLayer.prototype.onAdd.call(this,map);
         this._path = d3.geo.path().projection(function(d) {
             var point = map.latLngToLayerPoint(new L.LatLng(d[1],d[0]));
             return [point.x,point.y];
         });
-        this.on("tileunload",function(d) {
+        this.on('tileunload',function(d) {
             if (d.tile.xhr) d.tile.xhr.abort();
             if (d.tile.nodes) d.tile.nodes.remove();
             d.tile.nodes = null;
             d.tile.xhr = null;
         });
     },
-    _loadTile : function(tile,tilePoint) {
+   _loadTile : function(tile,tilePoint) {
         var self = this;
         this._adjustTilePoint(tilePoint);
- 
+
         if (!tile.nodes && !tile.xhr) {
-            tile.xhr = d3.json(this.getTileUrl(tilePoint),function(geoJson) {
-                tile.xhr = null;
-                tile.nodes = d3.select(map._container).select("svg").append("g");
-                tile.nodes.selectAll("path")
-                    .data(geoJson.features).enter()
-                .append("text")
-                .append('textPath')
-                    .attr("xlink:href", self.options.id2)
-                    .attr("class", self.options.class)
-                    .attr("console", self.options.console)
-                    .attr('startOffset',"50%")
-                    .text(self.options.text);
-                tile.nodes.selectAll("path")
-                    .data(geoJson.features).enter()
-                .append("path")
-                    .attr("d", self._path)
-                    .attr("class", self.options.class)
-                    .attr("style", self.options.style)
-                    .attr("id", self.options.id);
+            tile.xhr = d3.json(this.getTileUrl(tilePoint),function(error, tjData) {
+
+                var pois = topojson.feature(tjData, tjData.objects.pois);
+                var landUsages = topojson.feature(tjData, tjData.objects['land-usages']);
+                var buildings = topojson.feature(tjData, tjData.objects.buildings);
+                var road = topojson.feature(tjData, tjData.objects.highroad);
+                var water = topojson.feature(tjData, tjData.objects['water-areas']);
+                
+                tile.nodes = d3.select(map._container).select('svg').append('g');
+                tile.nodes.selectAll('path')
+
+                    .data(water.features).enter()
+                        .append('path')
+                            .attr('d', self._path)
+                            .attr('class','water')
+
+                    //** 
+                    //POIS are not cool right now
+                    //**
+                    // .data(pois.features).enter()
+                    //     .append('path')
+                    //         .attr('d', self._path)
+                    //         .attr('class', self.options.class)
+                    //         .attr('class',function(d){
+                    //             console.log(d.properties.kind)
+                    //         })
+
+                    .data(road.features).enter()
+                        .append('path')
+                            .attr('d', self._path)
+                            .attr('class',function(d){
+                                return d.properties.kind
+                            })
+
+                    .data(landUsages.features).enter()
+                        .append('path')
+                            .attr('d', self._path)
+                            .attr('class',function(d){
+                                if(d.properties.area < 6496310){
+                                    return d.properties.kind +' landuse'     
+                                }else{
+                                    return d.properties.kind +' landuse nodraw'     
+                                }   
+                            })
+
+                    .data(buildings.features).enter()
+                        .append('path')
+                            .attr('d', self._path)
+                            .attr('class','building')
+                    
+                    
             });
         }
     }
 });
+
+
